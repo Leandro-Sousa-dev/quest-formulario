a8dd669b61734e7019347bd99faa740f54fd41ca
Leandro Sousa <leandrosousa69outlook.com>
2023-02-22 16:08:36 -0300

    Pós-Correção

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,32 @@ const inputs = document.querySelectorAll('.campo')
 
 const btnEnviar = document.querySelector('.btn-enviar')
 
-btnEnviar.addEventListener('click', () => {
-    inputs.forEach(item => {
+const campoTelefone = document.getElementById('telefone')
+
+campoTelefone.addEventListener('keypress', (event) =>{   
+    if(event.key > '9' || event.key < '0'){
+        event.preventDefault()
+    }
+})
+
+
+btnEnviar.addEventListener('click', (event) => {
+    inputs.forEach(item => {       
+        const itemId = item.id
+        const Label = document.querySelector(`.${itemId}`)
+        
         if (item.value === '') {
+            event.preventDefault()
+            Label.classList.add('mostrar')
             item.classList.remove('preenchido')
             item.classList.add('vazio')
-        } else {
+               
+        }else{
+            Label.classList.remove('mostrar')
             item.classList.remove('vazio')
             item.classList.add('preenchido')
         }
+
     });
 })
+// item.id
